9f2f9afbc2c9958d123a0398e8e6918188c1cf20
DanielRodado <d4nielrodado@gmail.com>
2024-01-02 16:20:56 -0500

    Spring commit

---
+++ src/test/java/com/mindhub/AppCrud/AppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.AppCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
