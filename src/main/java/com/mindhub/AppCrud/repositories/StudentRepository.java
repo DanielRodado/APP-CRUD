4ee6ebabb3592563becaf1c380824470abd8bf8e
DanielRodado <d4nielrodado@gmail.com>
2024-01-03 15:41:51 -0500

    Creación de respositorios + Nueva entidad

---
+++ src/main/java/com/mindhub/AppCrud/repositories/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.mindhub.AppCrud.repositories;
+
+import com.mindhub.AppCrud.models.subClass.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
---
+++ src/main/java/com/mindhub/AppCrud/repositories/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.mindhub.AppCrud.repositories;
+
+import com.mindhub.AppCrud.models.subClass.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface TeacherRepository extends JpaRepository<Teacher, String> {
+}
