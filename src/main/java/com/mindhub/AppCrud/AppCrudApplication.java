5c5be0d58b1361e843bcacebf4042537557a035b
DanielRodado <d4nielrodado@gmail.com>
2024-01-03 16:51:50 -0500

    Más datos de prueba

--- src/main/java/com/mindhub/AppCrud/AppCrudApplication.java
+++ src/main/java/com/mindhub/AppCrud/AppCrudApplication.java
@@ -1,6 +1,8 @@
 package com.mindhub.AppCrud;
 
 import com.mindhub.AppCrud.models.subClass.Admin;
+import com.mindhub.AppCrud.models.subClass.Student;
+import com.mindhub.AppCrud.models.subClass.Teacher;
 import com.mindhub.AppCrud.repositories.AdminRepository;
 import com.mindhub.AppCrud.repositories.StudentRepository;
 import com.mindhub.AppCrud.repositories.TeacherRepository;
@@ -25,6 +27,11 @@ public class AppCrudApplication {
 			Admin adminOne = new Admin("Daniel", "Rodado", "danielrodado@gmail.com", "admin123");
 			adminRepository.save(adminOne);
 
+			Teacher teacherOne = new Teacher("Martin", "Araolaza", "martin@mentor.com", "mentor123");
+			teacherRepository.save(teacherOne);
+
+			Student studentOne = new Student("Esteban", "Girardot", "estebang@estudent.com", "student123");
+			studentRepository.save(studentOne);
 		};
 
 	}
