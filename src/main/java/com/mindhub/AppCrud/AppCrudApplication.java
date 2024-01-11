482953a8b112b6e6a9d04544329f4876366e1c9b
DanielRodado <d4nielrodado@gmail.com>
2024-01-11 16:23:35 -0500

    Modificación en los datos de prueba

--- src/main/java/com/mindhub/AppCrud/AppCrudApplication.java
+++ src/main/java/com/mindhub/AppCrud/AppCrudApplication.java
@@ -5,11 +5,13 @@ import com.mindhub.AppCrud.models.subClass.Admin;
 import com.mindhub.AppCrud.models.subClass.Student;
 import com.mindhub.AppCrud.models.subClass.Teacher;
 import com.mindhub.AppCrud.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cglib.core.Local;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -22,6 +24,9 @@ public class AppCrudApplication {
 		SpringApplication.run(AppCrudApplication.class, args);
 	}
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Bean
 	public CommandLineRunner initData(AdminRepository adminRepository, TeacherRepository teacherRepository,
 									  StudentRepository studentRepository, CourseRepository courseRepository,
@@ -30,7 +35,8 @@ public class AppCrudApplication {
 
 		return args -> {
 
-			Admin adminOne = new Admin("Daniel", "Rodado", "danielrodado@gmail.com", "admin123");
+			Admin adminOne = new Admin("Daniel", "Rodado", "danielrodado@admin.com",
+					passwordEncoder.encode("admin123"));
 			adminRepository.save(adminOne);
 
 			// Schedules
@@ -54,7 +60,7 @@ public class AppCrudApplication {
 			// Teachers and courses
 
 			Teacher teacherOne = new Teacher("Martin", "Araolaza", "martin@mentor.com",
-					"mentor123", List.of("Java", "Spring", "Spring Boot", "PostgreSQL"));
+					passwordEncoder.encode("mentor123"), List.of("Java", "Spring", "Spring Boot", "PostgreSQL"));
 			teacherRepository.save(teacherOne);
 
 			Course courseOne = new Course("Full Stack Java", "Cohort 51");
@@ -83,13 +89,16 @@ public class AppCrudApplication {
 
 			// Students
 
-			Student studentOne = new Student("Esteban", "Girardot", "estebang@student.com", "student123");
+			Student studentOne = new Student("Esteban", "Girardot", "estebang@student.com",
+					passwordEncoder.encode("student123"));
 			studentRepository.save(studentOne);
 
-			Student studentTwo = new Student("Alfredo", "Orozco", "alfred@student.com", "student123");
+			Student studentTwo = new Student("Alfredo", "Orozco", "alfred@student.com",
+					passwordEncoder.encode("student123"));
 			studentRepository.save(studentTwo);
 
-			Student studentThree = new Student("Felipe", "Risaralda", "felpr@student.com", "student123");
+			Student studentThree = new Student("Felipe", "Risaralda", "felpr@student.com",
+					passwordEncoder.encode("student123"));
 			studentRepository.save(studentThree);
 
 			// StudentCourses
